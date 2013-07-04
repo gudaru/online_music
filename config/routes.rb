1946ccfb1e6b815543bff6ed702004daeea556d4
gudaru <nguyendangluc91@gmail.com>
2013-07-04 18:24:06 +0700

    4 commit:hien thi nguoi dung

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 OnlineMusic::Application.routes.draw do
   get "users/new"
-
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
-
   get "static_pages/contact"
+
+  resources :users
 
   root to: 'static_pages#home'
 
