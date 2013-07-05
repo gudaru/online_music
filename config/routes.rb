f87055e3b2a07c9ef0e3dd442f453093b62c5af9
gudaru <nguyendangluc91@gmail.com>
2013-07-05 16:28:56 +0700

    7 commit: sign up//error loading page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 OnlineMusic::Application.routes.draw do
   # get "users/new"
-  get "static_pages/home"
+  # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
